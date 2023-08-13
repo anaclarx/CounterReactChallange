de94bb7093118127d5e10cb57722c22ff1f0797e
Ana Clara Granato <anaclaragranato@gmail.com>
2023-08-13 19:58:09 +0200

    adding box for informations and fixing alert and other functions

--- digital-ocean-tutorial/src/App.js
+++ digital-ocean-tutorial/src/App.js
@@ -1,35 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import goalImage from './goal.png';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [isFibonacci, setIsFibonacci] = useState(false);
   const [isApproachingThreshold, setIsApproachingThreshold] = useState(false);
   const [isThresholdExceeded, setIsThresholdExceeded] = useState(false);
+  const [goalPosition, setGoalPosition] = useState(0);
 
   const fibonacciCheck = (num) => {
-    if (num <= 1) return true;
-
-    let prev = 0;
-    let curr = 1;
-
-    while (curr <= num) {
-      if (curr === num) return true;
-      const next = prev + curr;
-      prev = curr;
-      curr = next;
+    if (num < 0) {
+      return false; 
     }
-
+  
+    let a = 0;
+    let b = 1;
+  
+    while (b <= num) {
+      if (b === num) {
+        return true;
+      }
+      
+      const next = a + b;
+      a = b;
+      b = next;
+    }
+  
     return false;
   };
 
+  const isPrimeNum = (num) => {
+      if (num < 0) {
+        return false;
+      }
+      
+      if (num <= 3) {
+        return true;
+      }
+      
+      if (num % 2 === 0 || num % 3 === 0) {
+        return false;
+      }
+      
+      for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) {
+          return false;
+        }
+      }
+      
+      return true; 
+  }
+
+  const isThreshold = (num) =>{
+    return fibonacciCheck(num) && num > 10 && isPrimeNum(num);
+  }
+
+
+
   useEffect(() => {
     setIsFibonacci(fibonacciCheck(counter));
-    
-    const threshold = 10; // Defina o limiar desejado
-    setIsApproachingThreshold(counter >= threshold - 3 && counter < threshold);
-    setIsThresholdExceeded(counter >= threshold);
-  }, [counter]);
+    const threshold = 13;
+
+    if(isThreshold(counter + 3)){
+      setIsApproachingThreshold(true);
+    }
+
+    if(isThreshold(counter)){
+      setIsThresholdExceeded(isThreshold(counter))
+    }
+
+    if (isFibonacci) {
+      const newPosition = Math.min(100, goalPosition + 10);
+      setGoalPosition(newPosition);
+    }
+  }, [counter, isFibonacci, goalPosition]);
 
   const incrementCounter = () => {
     if (!isThresholdExceeded) {
@@ -39,25 +84,51 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>Contador Fibonacci</h1>
+      <h1>Fibonacci Counter</h1>
       <div className="counter">
-        <div className={`counter-value ${isFibonacci ? 'fibonacci' : ''}`}>
-          {counter}
+        <div className="counter-value">
+          {counter === -1
+            ? 'Warning: Approaching the special Fibonacci number...'
+            : counter}
         </div>
-        {isApproachingThreshold && !isThresholdExceeded && (
-          <div className="approaching-alert">
-            Aproximando-se do limiar!
-          </div>
-        )}
-        {isThresholdExceeded && (
-          <div className="threshold-exceeded">
-            Limite excedido! Não é possível incrementar mais.
-          </div>
-        )}
+        <div className={`fibonacci-sign ${isFibonacci ? 'visible' : ''}`}>
+          Fibonacci Number!
+        </div>
+      </div>
+      {isApproachingThreshold && !isThresholdExceeded && (
+        <div className="approaching-alert">
+          Approaching the threshold!
+        </div>
+      )}
+      {isThresholdExceeded && (
+        <div className="threshold-exceeded">
+          Threshold exceeded! Cannot increment further.
+        </div>
+      )}
+      <div className="progress-bar">
+        <div className="goal-container" style={{ left: `${goalPosition}%` }}>
+          <img src={goalImage} alt="goal" className="goal" />
+        </div>
       </div>
       <button onClick={incrementCounter} disabled={isThresholdExceeded}>
-        Incrementar
+        Increment
       </button>
+      <div className="right-content">
+        <div className="info-box">
+          <h2>What is the Fibonacci Sequence?</h2>
+          <p>
+            The Fibonacci Sequence is a series of numbers in which each number is the sum of the two preceding ones.
+            For example, the sequence starts as 0, 1, 1, 2, 3, 5, 8, 13, and so on.
+          </p>
+        </div>
+        <div className="info-box">
+          <h2>What is the Threshold?</h2>
+          <p>
+            The threshold is the frist prime number from the Fibonacci Sequence above 10.
+            If you want to restart the counter re-load the page.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
